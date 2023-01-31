496f27a1d92ed3586abd17e7b0e6e0b0d55ba359
Valentina Ruiz <v.zruiz06@gmail.com>
2023-01-30 16:13:16 -0800

    added password column to user model

--- app/models/User.py
+++ app/models/User.py
@@ -2,5 +2,6 @@ from app import db
 
 class User(db.Model):
   user_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-  name = db.Column(db.String(20))
+  username = db.Column(db.String(20))
+  password = db.Column(db.String)
   workouts = db.relationship('Workout', secondary='user_workouts', backref='users')
